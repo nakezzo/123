2fef53e85f1c182bd12210ec8213bbb15cdb648d
nakezzo <ivanshemuda2@gmail.com>
2024-10-27 22:32:30 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TOKEN = "@end_soft" #токен BotFather
-CRYPTO_TOKEN = "@end_soft" #токен CreptoPay
-LOG_CHANNEL = -1002138027738 #ЛОГИРОВАНИЕ
-CHANNEL_BROKER = -1002138027738 #Посредник
-MAIN_CHANNEL= -1002138027738 #основа
-ADMINS = [5382909825] #список админов
-CHECK_URL = "https://t.me/send?start=IVZuUn1UbdsQ" #юрл счёта
-MONEYBACK = 10 #процент манибэка от игры
+TOKEN = "7695712879:AAGGRCMdWia5__Gdvdc2LdS2SfVONmYZrMM" #токен BotFather
+CRYPTO_TOKEN = "285684:AAf34SctGScqfoBkOOot1GFhhuOHVNcplPH" #токен CreptoPay
+LOG_CHANNEL = --1002333504858 #ЛОГИРОВАНИЕ
+CHANNEL_BROKER = --1002333504858 #Посредник
+MAIN_CHANNEL= --1002333504858 #основа
+ADMINS = [6258165965] #список админов
+CHECK_URL = "t.me/send?start=IVw57jH45aBY" #юрл счёта
+MONEYBACK = 1 #процент манибэка от игры
